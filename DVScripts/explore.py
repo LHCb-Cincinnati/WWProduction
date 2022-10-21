3c32e4a02a2c601878da054699e71035298a80ab
ganowak <ganowak@cern.ch>
2022-10-21 13:49:14 -0400

    Refactored explore.py to use GaudiPython.

--- DVScripts/explore.py
+++ DVScripts/explore.py
@@ -1,54 +1,71 @@
+# Imports
 import sys
+import pdb
+import inspect
+from collections import OrderedDict
 
-import GaudiPython as GP
+import numpy as np
+import ROOT
+
+import GaudiPython
 from GaudiConf import IOHelper
 from Configurables import DaVinci
+from Configurables import ApplicationMgr 
 
-dv = DaVinci()
-dv.DataType = '2016'
-dv.Simulation = True
+# Configure DaVinci
+DaVinci().InputType = 'DST'
+DaVinci().PrintFreq = 1000
+DaVinci().DataType = '2016'
+DaVinci().Simulation = True
+# Only ask for luminosity information when not using simulated data
+DaVinci().Lumi = not DaVinci().Simulation
+DaVinci().CondDBtag = 'sim-20170721-2-vc-md100'
+DaVinci().DDDBtag = 'dddb-20170721-3'
+# DaVinci().CondDBtag = 'sim-20161124-2-vc-md100'
+# DaVinci().DDDBtag = 'dddb-20150724'
 
-# Retrieve file path to open as the last command line argument
-inputFiles = [sys.argv[-1]]
-IOHelper('ROOT').inputFiles(inputFiles)
+# Use the local input data
+WW_data = '00057387_00000003_3.AllStreams.dst'
+tautau_data = '00144995_00000138_7.AllStreams.dst'
+IOHelper().inputFiles([
+    '~/WWProduction/Data/TestFiles/' + tautau_data
+], clear=True)
 
-appMgr = GP.AppMgr()
-evt = appMgr.evtsvc()
 
-appMgr.run(1)
+# Configure GaudiPython
+gaudi = GaudiPython.AppMgr()
+tes   = gaudi.evtsvc()
 
-def advance(decision):
-    """Advance until stripping decision is true, returns
-    number of events by which we advanced"""
-    n = 0
-    while evt['/Event']:
-        reports = evt['/Event/Strip/Phys/DecReports']
-        if reports.hasDecisionName('Stripping{}Decision'.format(decision)):
-            break
+genTool = gaudi.toolsvc().create(
+    'DaVinciSmartAssociator',
+    interface = 'IParticle2MCWeightedAssociator')
 
-        appMgr.run(1)
-        n += 1
-    return n
+# Run
+evtnum = 0
+gaudi.run(1)
+evtmax = 100
+ele_counter = 0
+muon_counter = 0
+hadron_counter = 0
 
-def get_candidates(stripping_line):
-    candidates = evt['/Event/AllStreams/Phys/{}/Particles'.format(stripping_line)]
-    return(candidates)
+# while (evtnum<evtmax):
+while bool(tes['/Event']):
+    evtnum += 1
 
-def quick_explore(decision):
-    n=0
-    print_count = 0
-    while (evt['/Event'] and print_count<100):
-        reports = evt['/Event/Strip/Phys/DecReports']
-        if reports.hasDecisionName('Stripping{}Decision'.format(decision)):
-            candidates = get_candidates(decision)
+    # Truth Stuff
+    # print('New Event:')
+    truth_particles = tes['MC/Particles']
+    tau_list = [particle for particle in truth_particles if particle.particleID().pid()==15]
+    decay_list = [particle.particleID().pid() for particle in tau_list[0].endVertices()[0].products()]
+    # print(decay_list)
+    if -12 in decay_list:
+        ele_counter+=1
+    elif 13 in decay_list:
+        muon_counter+=1
+    else:
+        hadron_counter+=1
+    gaudi.run(1) 
 
-            # Print the candidates for the event
-            print("###########################\n")
-            print(f"Event: {n}.  Number of Candidates: {len(candidates)}\n")
-            for candidate in candidates:
-                print(candidate)
-            print_count+=1
-            print("###########################\n")
-        appMgr.run(1)
-        n+=1
-    return n
+print(f'Electron Count: {ele_counter}')
+print(f'Muon Count: {muon_counter}')
+print(f'Hadron Count: {hadron_counter}')
