058ca6f967c694007c43a543b3fed881da3504f6
ganowak <ganowak@cern.ch>
2022-10-24 15:25:00 -0400

    More CrossSection Testing.

--- GenLevelStudies/pythia/analysis.py
+++ GenLevelStudies/pythia/analysis.py
@@ -1,7 +1,7 @@
 # Imports
 import sys
 import os
-import argparse
+import pdb
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,38 +12,9 @@ import vector
 sys.path.append('../../Analysis') # This is Awesome!
 import analysis_tools as at
 
-
-# Argument Parser
-parser = argparse.ArgumentParser(description='''Process files and settings for analysis.
-                                Required arguments are: input_files.
-                                Optional arguments are: cross_section, testing.
-                                Ex: python3 analysis.py test.root -c 53.5 -p''')
-parser.add_argument('input_files',type=open, nargs='+',
-                    help='The file or files to be anayzed. Input files should be root files.')
-parser.add_argument('-c', '--cross_section', type=float, default=False, nargs='*', 
-                    help='''The cross section used to normalize histograms in fb.
-                    This option should either not be specified or have exactly
-                    as many arguments as input files.''')
-parser.add_argument('-t', '--testing', default=True, action='store_true',
-                    help='''Flag to indicate if a new output folder should be created for 
-                    this analysis.  -t means that all plots will go in folder labelled Test.''')
-parser.add_argument('-p', '--production', dest='testing', action='store_false',
-                    help='''Flag to indicate if a new output folder should be created for this 
-                    analysis.  -p means that all plots will be put into a new output folder with
-                    the same name as the input file.''')
-args = parser.parse_args()
-print(args)
-
-# Make sure the cross-sections have as many aguments as files.
-if (not args.cross_section):
-    args.cross_section = [False] * len(args.input_files)
-elif (len(args.cross_section) != len(args.input_files)):
-    raise RuntimeError('''The number of given cross sections is different from
-                       the number of given input files.  Please either don't
-                       specify a cross section arugment or specify as many
-                       cross sections as there are input files.''')
-
-# Parse inputs
+# Parse Inputs
+parser = at.Parser(sys.argv[1:])
+args = parser.args
 file_name =  args.input_files[0].name
 cross_section = args.cross_section[0] # Cross section in fb
 
@@ -51,13 +22,13 @@ cross_section = args.cross_section[0] # Cross section in fb
 ifile = uproot.open(file_name)
 tree = ifile['AnalysisTree'].arrays()
 
-# # Create Vectors
-
+# Create Vectors
 lminus_vec = vector.zip({
     'px': tree['LeptonMinus'].px,
     'py': tree['LeptonMinus'].py,
     'pz': tree['LeptonMinus'].pz,
     'e': tree['LeptonMinus'].energy,
+    'pid': tree['LeptonMinus'].id
 })
 
 lplus_vec = vector.zip({
@@ -65,59 +36,63 @@ lplus_vec = vector.zip({
     'py': tree['LeptonPlus'].py,
     'pz': tree['LeptonPlus'].pz,
     'e': tree['LeptonPlus'].energy,
+    'pid': tree['LeptonPlus'].id
 })
 dilepton_vec = lminus_vec + lplus_vec
+muon_vec = ak.where((abs(lminus_vec.pid)==13), lminus_vec, lplus_vec)
+electron_vec = ak.where((abs(lminus_vec.pid)==11), lminus_vec, lplus_vec)
+leading_lepton_vec = ak.where((lplus_vec.pt>lminus_vec.pt), lplus_vec, lminus_vec)
+trailing_lepton_vec = ak.where((lplus_vec.pt<lminus_vec.pt), lplus_vec, lminus_vec)
 
-# Cuts
-lepton_eta_cut = ((lminus_vec.eta > 2) & (lminus_vec.eta<5)
+# Masks
+both_lepton_acc_mask = ((lminus_vec.eta > 2) & (lminus_vec.eta<5)
                     & (lplus_vec.eta > 2) & (lplus_vec.eta<5))
-high_pT_lepton_cut = ((lminus_vec.pt>15) | (lplus_vec.pt>15))
-low_pT_lepton_cut = ((lminus_vec.pt>5) & (lplus_vec.pt>5))
-muon_pid_cut = ((tree['LeptonMinus'].id==13) & (tree['LeptonPlus'].id==-13))
-invariant_mass_cut = (dilepton_vec.m>10)
-lepton_cuts = lepton_eta_cut&high_pT_lepton_cut&low_pT_lepton_cut&muon_pid_cut
-drellyan_cuts = lepton_eta_cut&low_pT_lepton_cut&invariant_mass_cut
+high_pT_lepton_mask = ((lminus_vec.pt>15) | (lplus_vec.pt>15))
+low_pT_lepton_mask = ((lminus_vec.pt>5) & (lplus_vec.pt>5))
+mumu_decay_mask = ((tree['LeptonMinus'].id==13) & (tree['LeptonPlus'].id==-13))
+mue_decay_mask = (((lminus_vec.pid==13) & (lplus_vec.pid==-11))
+                    | ((lminus_vec.pid==11) & (lplus_vec.pid==-13)))
+one_lepton_acc_mask = (((lminus_vec.eta>2) & (lminus_vec.eta<5))
+                        | ((lplus_vec.eta>2) & (lplus_vec.eta<5)))
+invariant_mass_mask = (dilepton_vec.m>10)
+lepton_mask = both_lepton_acc_mask&high_pT_lepton_mask&low_pT_lepton_mask&mumu_decay_mask
+drellyan_mask = both_lepton_acc_mask&low_pT_lepton_mask&invariant_mass_mask
+
+# Apply Masks
+leading_lepton_vec = leading_lepton_vec[one_lepton_acc_mask&high_pT_lepton_mask]
+trailing_lepton_vec = trailing_lepton_vec[one_lepton_acc_mask&high_pT_lepton_mask]
 
 # Calculate Quantitites
-greater_pt_array = (lminus_vec.pt > lplus_vec.pt)
-lesser_pt_array = -1*(greater_pt_array-1)
-leading_lepton_pT_array = greater_pt_array * lminus_vec.pt + lesser_pt_array * lplus_vec.pt
-trailing_lepton_pT_array = lesser_pt_array * lminus_vec.pt + greater_pt_array * lplus_vec.pt
-delta_phi_array = np.abs(lminus_vec.deltaphi(lplus_vec))
-delta_eta_array = np.abs(lminus_vec.deltaeta(lplus_vec))
-delta_r_array = np.abs(lminus_vec.deltaR(lplus_vec))
+delta_phi_array = np.abs(leading_lepton_vec.deltaphi(trailing_lepton_vec))
+delta_eta_array = np.abs(leading_lepton_vec.deltaeta(trailing_lepton_vec))
+delta_r_array = np.abs(leading_lepton_vec.deltaR(trailing_lepton_vec))
 
 # # Create output directory if it does not yet exist
 # # and change current directory to output directory
 #file_path = at.create_folder_path('WW_GenLevelPythia' + '.root', args.testing)
 
-file_path = 'DY_GenLevelPythia'
+file_path = 'WW_GenLevelPythia'
 #os.mkdir(file_path)
 os.chdir(file_path)
 
 # scale_factor
-#cross_section = 3038 # in fb (WW)
-cross_section = 6.577E6 # in fb (DY)
-nevents = 100000
-scale_factor = cross_section / nevents
-
 
 # Plots
-at.create_hist(dilepton_vec.m[drellyan_cuts], 'DiLepton Mass', bins=50, range=(0,150),
-            weights=[scale_factor]*len(dilepton_vec[drellyan_cuts]))
-at.create_hist(dilepton_vec.pt[drellyan_cuts], 'Lepton Pair pT', yscale='log', bins=50, range=(0,150),
-            weights=[scale_factor]*len(dilepton_vec[drellyan_cuts]))
-at.create_hist(leading_lepton_pT_array[drellyan_cuts], 'Leading Lepton pT', yscale='log', bins=50, range=(0,150),
-            weights=[scale_factor]*len(dilepton_vec[drellyan_cuts]))
-at.create_hist(trailing_lepton_pT_array[drellyan_cuts], 'Trailing Lepton pT', yscale='log', bins=50, range=(0,150),
-            weights=[scale_factor]*len(dilepton_vec[drellyan_cuts]))
-at.create_hist(lminus_vec.pt[drellyan_cuts], 'lminus pT', bins=50, yscale='log', range=(0,150),
-            weights=[scale_factor]*len(dilepton_vec[drellyan_cuts]))
-at.create_hist(lplus_vec.pt[drellyan_cuts], 'lplus pT', yscale='log', bins=50, range=(0,150),
-            weights=[scale_factor]*len(dilepton_vec[drellyan_cuts]))
-at.create_hist(delta_phi_array[drellyan_cuts], 'Delta Phi', bins=50,
-            weights=[scale_factor]*len(dilepton_vec[drellyan_cuts]))
-at.create_hist(delta_r_array[drellyan_cuts], 'Delta R', bins=50,
-            weights=[scale_factor]*len(dilepton_vec[drellyan_cuts]))
-at.create_hist(delta_eta_array[drellyan_cuts], 'Delta Eta', bins=50,
-            weights=[scale_factor]*len(dilepton_vec[drellyan_cuts]))
+at.create_hist(leading_lepton_vec.pt, 'StandAlone Leading Lepton Pt', bins=100, range=(0,200),
+                density=True)
+at.create_hist(trailing_lepton_vec.pt, 'StandAlone Trailing Lepton Pt', bins=100, range=(0,200),
+                density=True)
+at.create_hist(leading_lepton_vec.eta, 'StandAlone Leading Lepton Eta', bins=50, range=(-7,7),
+                density=True)
+at.create_hist(trailing_lepton_vec.eta, 'StandAlone Trailing Lepton Eta', bins=50, range=(-7,7),
+                density=True)
+at.create_hist(dilepton_vec.m, 'StandAlone DiLepton Mass', bins=100, range=(0,200),
+                density=True)
+at.create_hist(dilepton_vec.pt, 'StandAlone DiLepton pT', bins=100, range=(0,200),
+                density=True)
+at.create_hist(delta_phi_array, 'StandAlone Delta Phi', bins=50,
+                density=True)
+at.create_hist(delta_r_array, 'StandAlone Delta R', bins=50, range=(0, 4),
+                density=True)
+at.create_hist(delta_eta_array, 'StandAlone Delta Eta', bins=50, range=(0, 3),
+                density=True)
