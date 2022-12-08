eff50e4c845c863c95673ae63428d45e74f84ac0
ganowak <ganowak@cern.ch>
2022-12-08 05:51:02 -0500

    Refactored efficiency code with new id arrays.

--- Analysis/EfficiencyStudies.py
+++ Analysis/EfficiencyStudies.py
@@ -29,44 +29,70 @@ truth_leading_lepton_key = 'tLeading_lepton_array'
 truth_trailing_lepton_key = 'tTrailing_lepton_array'
 reco_leading_lepton_key = 'rLeading_lepton_array'
 reco_trailing_lepton_key = 'rTrailing_lepton_array'
+reco_leading_lepton_id_key = 'rTrailing_lepton_id_array'
+reco_trailing_lepton_id_key = 'rTrailing_lepton_id_array'
 
 # Create Vectors/Arrays
+# Truth Vectors
 tLeading_lepton_vec = vector.awk(tree[truth_leading_lepton_key].array())
 tTrailing_lepton_vec = vector.awk(tree[truth_trailing_lepton_key].array())
 tDilepton_vec = tLeading_lepton_vec + tTrailing_lepton_vec
+tMuon_vec = ak.where((abs(tLeading_lepton_vec.pid)==13),
+                    tLeading_lepton_vec, tTrailing_lepton_vec)
+tElectron_vec = ak.where((abs(tLeading_lepton_vec.pid)==11),
+                        tLeading_lepton_vec, tTrailing_lepton_vec)
+# Reconstructed Vectors
 rLeading_lepton_vec = vector.awk(tree[reco_leading_lepton_key].array())
 rTrailing_lepton_vec = vector.awk(tree[reco_trailing_lepton_key].array())
 rDilepton_vec = rLeading_lepton_vec + rTrailing_lepton_vec
-rMuonid_array = tree['rMuonid_array'].array()
+rLeading_lepton_id_array = tree[reco_leading_lepton_id_key].array()
+rTrailing_lepton_id_array = tree[reco_trailing_lepton_id_key].array()
+rMuon_vec = ak.where((abs(rLeading_lepton_vec.pid)==13),
+                    rLeading_lepton_vec, rTrailing_lepton_vec)
+rElectron_vec = ak.where((abs(rLeading_lepton_vec.pid)==11),
+                        rLeading_lepton_vec, rTrailing_lepton_vec)
+rMuon_id_array = ak.where((abs(rLeading_lepton_vec.pid)==13),
+                    rLeading_lepton_id_array, rTrailing_lepton_id_array)
+rElectron_id_array = ak.where((abs(rLeading_lepton_vec.pid)==11),
+                    rLeading_lepton_id_array, rTrailing_lepton_id_array)
 
 # Masks
-tEvt_num = tree['tEvt_num'].array().to_list()
-rEvt_num = tree['rEvt_num'].array().to_list()
+tEvt_num = tree['tEvt_num'].array()
+rEvt_num = tree['rEvt_num'].array()
 truth_lepton_acceptance_mask = ((tLeading_lepton_vec.eta>2)
                                 & (tLeading_lepton_vec.eta<5)
                                 & (tTrailing_lepton_vec.eta>2)
                                 & (tTrailing_lepton_vec.eta<5))
 process_mask = (tree['tDecay_process_array'].array()['mue'] == 1)
-muonid_mask = (tree['tDecay_process_array'].array()['mue'] == 1)
-good_reco_event_mask = [process_mask[tEvt_num.index(evt)] for evt in rEvt_num]
+good_reco_event_mask = [process_mask[np.where(tEvt_num==evt)[0][0]] for evt in rEvt_num]
 full_truth_mask = process_mask & truth_lepton_acceptance_mask
 
-# Mask Vectors and Extract Lepton Info
+# Mask Vectors
 # Truth Vectors
+tEvt_num = tEvt_num[full_truth_mask]
 tLeading_lepton_vec = tLeading_lepton_vec[full_truth_mask]
 tTrailing_lepton_vec = tTrailing_lepton_vec[full_truth_mask]
 tDilepton_vec = tDilepton_vec[full_truth_mask]
-tMuon_vec = ak.where((abs(tLeading_lepton_vec.pid)==13), tLeading_lepton_vec, tTrailing_lepton_vec)
-tElectron_vec = ak.where((abs(tLeading_lepton_vec.pid)==11), tLeading_lepton_vec, tTrailing_lepton_vec)
+tMuon_vec = tMuon_vec[full_truth_mask]
+tElectron_vec = tElectron_vec[full_truth_mask]
 # Reco Vectors
+rEvt_num = rEvt_num[good_reco_event_mask]
 rLeading_lepton_vec = rLeading_lepton_vec[good_reco_event_mask]
 rTrailing_lepton_vec = rTrailing_lepton_vec[good_reco_event_mask]
+rLeading_lepton_id_array = rLeading_lepton_id_array[good_reco_event_mask]
+rTrailing_lepton_id_array = rTrailing_lepton_id_array[good_reco_event_mask]
 rDilepton_vec = rDilepton_vec[good_reco_event_mask]
-rMuon_vec = ak.where((abs(rLeading_lepton_vec.pid)==13), rLeading_lepton_vec, rTrailing_lepton_vec)
-rElectron_vec = ak.where((abs(rLeading_lepton_vec.pid)==11), rLeading_lepton_vec, rTrailing_lepton_vec)
-# Clean this up
-rMuonid_array = ak.where((abs(rLeading_lepton_vec.pid)==13), rMuonid_array['leading'][good_reco_event_mask], rMuonid_array['trailing'][good_reco_event_mask])
-rTightmuon_vec = rMuon_vec[rMuonid_array]
+rMuon_vec = rMuon_vec[good_reco_event_mask]
+rElectron_vec = rElectron_vec[good_reco_event_mask]
+rMuon_id_array = rMuon_id_array[good_reco_event_mask]
+rElectron_id_array = rElectron_id_array[good_reco_event_mask]
+
+for i in range(len(rEvt_num)):
+    if i==3:
+        continue
+    index = np.where(tEvt_num==rEvt_num[i])[0][0]
+    at.DeltaRMatchBool(rMuon_vec[i], tMuon_vec[index])
+pdb.set_trace()
 
 # Create output directory if it does not yet exist
 # and change current directory to output directory
